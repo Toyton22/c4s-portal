fc246965188e5584a816346bf2f24be230905ddf
Toyton22 <toyton.niscev@gmail.com>
2024-03-12 22:40:25 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <MainHeader />
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MainHeader from './components/MainHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MainHeader
+  },
+  computed: {
+    responsive() {
+      return (window.innerWidth < 750)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- ヘッダー -->
+  <div class="bg-dark">
+    <div class="mw-xl mx-auto">
+      <div class="p-3 hover pointer mw-logo" onclick="location.href=`/`">
+        <img src="@/assets/portal2024_logo.svg" class="w-100">
+      </div>
+      <div v-if="!responsive" class="text-end">
+        <span class="text-white hover pointer">トップ</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainHeader",
+  data() { return {
+    pages: [
+      {
+        title: "トップ",
+        path: "/"
+      }
+    ]
+  }}
+}
+</script>
+
+<style>
+.mw-logo {
+  max-width: 180px;
+}
+</style>
